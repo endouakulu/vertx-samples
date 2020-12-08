19d5bce90be78a82ff14b37410483af0e152fc9c
Eric Ndouakulu Kiaku Mbuta <eric.ndouakulu@mirakl.com>
2020-12-08 15:42:13 +0100

    Clean up

--- src/main/java/com/endouakulu/helloworld/MainVerticle.java
+++ src/main/java/com/endouakulu/helloworld/MainVerticle.java
@@ -6,12 +6,10 @@ import io.vertx.core.Promise;
 public class MainVerticle extends AbstractVerticle {
 
     @Override
-    public void start(Promise<Void> startPromise) throws Exception {
-        vertx.createHttpServer().requestHandler(req -> {
-            req.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x!");
-        }).listen(8888, http -> {
+    public void start(Promise<Void> startPromise) {
+        vertx.createHttpServer().requestHandler(req -> req.response()
+                .putHeader("content-type", "text/plain")
+                .end("Hello from Vert.x!")).listen(8888, http -> {
             if (http.succeeded()) {
                 startPromise.complete();
                 System.out.println("HTTP server started on port 8888");
